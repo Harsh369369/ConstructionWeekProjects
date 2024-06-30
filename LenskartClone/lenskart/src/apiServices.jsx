12fbb5c50764791c61394940a14b50f89062eb38
unknown <harshgera003@gmail.com>
2024-06-30 23:28:05 +0530

    commited

--- LenskartClone/lenskart/src/apiServices.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000';
-
-export const getSections = async () => {
-  const response = await axios.get(`${API_URL}/sections`);
-  return response.data;
-};
